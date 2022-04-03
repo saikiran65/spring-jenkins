467e6f11c71c65c98630363dfa3fbee8abf336bd
nkirangoud <nkirangoud1@gmail.com>
2022-04-03 23:57:42 +0530

    second commit

--- src/main/java/in/ashokit/beans/Bike.java
+++ src/main/java/in/ashokit/beans/Bike.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class Bike implements ApplicationRunner {
 
 	public Bike() {
-		System.out.println("Bike started");
+		System.out.println("Bike is started");
 	}
 	
 	@Override
